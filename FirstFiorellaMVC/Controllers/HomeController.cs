74501b9f4123a6429cfd82877192819b9cd790ff
Namik Heydarov <heydarovnamiq@gmail.com>
2022-02-09 01:50:18 +0400

    Add project files.

---
+++ FirstFiorellaMVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using FirstFiorellaMVC.DataAccessLayer;
+using FirstFiorellaMVC.Models;
+using FirstFiorellaMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace FirstFiorellaMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public HomeController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new HomeViewModel()
+            {
+                Products = _appDbContext.Products.Include(x=>x.Images).Take(8).ToList(),
+                Categories = _appDbContext.Categories.ToList(),
+                Slider = _appDbContext.Sliders.SingleOrDefault(),
+                SliderImages = _appDbContext.SliderImages.ToList(),
+                AboutImage = _appDbContext.AboutImages.SingleOrDefault(),
+                AboutContext = _appDbContext.AboutContexts.SingleOrDefault(),
+                AboutUnstyledLists = _appDbContext.AboutUnstyledLists.ToList(),
+                Experts = _appDbContext.Experts.Include(x => x.Position).ToList(),
+                ExpertContext = _appDbContext.ExpertContexts.SingleOrDefault(),
+                Positions = _appDbContext.Positions.ToList(),
+                Subcribe = _appDbContext.Subcribes.SingleOrDefault(),
+                Blogs = _appDbContext.Blogs.ToList(),
+                BlogContext = _appDbContext.BlogContexts.SingleOrDefault(),
+                Authors = _appDbContext.Authors.ToList(),
+                Instagrams = _appDbContext.Instagrams.ToList(),
+                Socials = _appDbContext.Socials.ToList(),
+                Campaigns = _appDbContext.Campaigns.ToList(),
+            });
+        }
+    }
+}
---
+++ FirstFiorellaMVC/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace FirstFiorellaMVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Dimension { get; set; }
+
+        public string Weight { get; set; }
+
+        public string SKUCode { get; set; }
+
+        public  List<ProductImage> Images{ get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public int? CampaignId { get; set; }
+
+        public Campaign Campaign { get; set; }
+    }
+}
